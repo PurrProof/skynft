bbfab9b9d5b9573a562027254b500849c3aa0240
PurrProof <>
2024-01-06 16:27:02 +0100

    wip: skynft page/components

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -3,6 +3,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages:["@SkyNft/sdk"],
   typescript: {
     ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
   },
--- packages/nextjs/pages/skynft.tsx
+++ packages/nextjs/pages/skynft.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import type { NextPage } from "next";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { MetaHeader } from "~~/components/MetaHeader";
+import { SkyNftTest } from "~~/components/skynft/SkyNftTest";
 
 const SkyNft: NextPage = () => {
   return (
@@ -9,6 +10,7 @@ const SkyNft: NextPage = () => {
       <MetaHeader />
 
       <div>Hello, World!</div>
+      <SkyNftTest />
     </>
   );
 };
